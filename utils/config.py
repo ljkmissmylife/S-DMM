1b34f3364a2bf5502f567fbdd8660302719c43ea
Pedro Vieira <pedrovieira@arrowplan.com>
2021-10-20 12:06:49 +0200

    Adapt config file to S-DMM needs

--- utils/config.py
+++ utils/config.py
@@ -27,12 +27,11 @@ class SDMMConfig:
             self.train_split = cfg['train_split']
             self.train_batch_size = cfg['train_batch_size']
             self.test_batch_size = cfg['test_batch_size']
-            self.sample_size = cfg['sample_size']
-            self.sample_bands = cfg['sample_bands']
             self.generate_samples = cfg['generate_samples']
             self.max_samples = cfg['max_samples']
 
             # Hyper parameters
+            self.sample_size = cfg['sample_size']
             self.num_runs = cfg['num_runs']
             self.num_epochs = cfg['num_epochs']
             self.learning_rate = cfg['learning_rate']
@@ -49,7 +48,6 @@ class SDMMConfig:
             self.use_checkpoint = cfg['use_checkpoint']
             self.print_frequency = cfg['print_frequency']
             self.use_tensorboard = cfg['use_tensorboard']
-            self.write_frequency = cfg['write_frequency']
             if self.use_tensorboard:
                 self.tensorboard_folder = self.exec_folder + 'tensorboard/'
 
